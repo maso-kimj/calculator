db4aabf55fc2f84edd5b3f2a56a0b62e9c3db8db
kimj <kimj@moneymagnet.co.kr>
2016-01-03 22:03:54 +0900

    fixed #1 스켈레톤 작성

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+	static int add(int a, int b) {
+		return a + b;
+	}
+
+
+
+	public static void main(String args[]) {		
+		System.out.println("덧셈 : " + add(1, 2));
+	}
+}
+
